b872e7f384e4cdb212325abdc971bf0a0c4a1df6
Flor Espino <florencia.espino@gmail.com>
2021-11-22 22:07:35 -0300

    hello world

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Un michi" src="./assets/michi.png">
+    <HelloWorld msg="Holas vienen, holas van"/>
   </div>
 </template>
 
@@ -24,5 +24,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img {
+  height: 300px;
+  width: auto;
 }
 </style>
